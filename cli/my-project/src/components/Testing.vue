97682d1e00716bfabbc2216985f6875d0f98c51c
Moekyaw97 <moethihakyaw15@gmail.com>
2020-09-29 15:40:58 +0630

    first commit

---
+++ cli/my-project/src/components/Testing.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="testing">
+		<h1>Hello Testing vue</h1>
+	</div>
+
+</template>
+
+
+<script type="text/javascript">
+	
+
+</script>
+
+<style type="text/css">
+	
+
+</style>
