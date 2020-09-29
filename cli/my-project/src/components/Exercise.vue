75e7c85d9a88e9e5ab7a78558e4a1d161c1e12d8
Moekyaw97 <moethihakyaw15@gmail.com>
2020-09-29 17:47:48 +0630

    update

--- cli/my-project/src/components/Exercise.vue
+++ cli/my-project/src/components/Exercise.vue
@@ -1,13 +1,46 @@
 <template>
 	<div class="exercise">
-		<h1 class="text-danger">This is Exercise Component</h1>
+
+
+		<div class="row">
+			<div class="col-md-3 my-3" v-for="(tag,index) in tags" :key="index">
+				<b-card :header="tag.tag_name" class="text-center">
+					<b-card-text>{{tag.tag_description.substring(0,50)+"..."}}</b-card-text>
+				</b-card>
+				
+			</div>
+		</div>
 	</div>
+
 </template>
 
+
 <script type="text/javascript">
-	
+	import MyService from '@/services/MyService.js'
+export default{
+	data(){
+		return{
+			tags:[]
+		}
+	},
+	mounted(){
+		this.getAllTags()
+	},
+	methods:{
+		getAllTags(){
+			MyService.getTags()
+			.then(res=>{
+				this.tags = res.data.tags
+			})
+			.catch(err=>{
+				console.error(err)
+			})
+		}
+	}
+}
 </script>
 
 <style type="text/css">
 	
+
 </style>
